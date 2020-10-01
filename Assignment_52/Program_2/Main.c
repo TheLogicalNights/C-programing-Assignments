ce255874f21e906a67b6e543856861d34a3cf275
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2020-10-01 22:19:06 +0530

    Add files via upload

---
+++ Assignment_52/Program_2/Main.c
@@ -0,0 +1,18 @@
+/*
+	Problem Statement : 	Write a program which accept file name which contains information of
+				student and display the information of student having lowest marks.
+*/
+
+#include "Header.h"
+
+int main()
+{
+	char filename[80] = {'\0'};
+	
+	printf("Enter filename\n");
+	scanf(" %s",filename);
+	
+	Display(filename);
+	
+	return 0;
+}
