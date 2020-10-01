f72856e3d0c6d8ae2aefb977b51fdf7fe66a61ed
Swapnil Ramesh Adhav <69031236+TheLogicalNights@users.noreply.github.com>
2020-10-01 22:18:20 +0530

    Add files via upload

---
+++ Assignment_52/Program_1/Helper.c
@@ -0,0 +1,58 @@
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//
+// Function Name : Display
+// Parameters    : String
+// Return Value  : None
+// Description   : It takes filename as input and display only information of that student having highest marks from file
+// Author        : Swapnil Ramesh Adhav
+// Date          : 27th Sept 2020
+//
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+#include "Header.h"
+
+void Display(char *filename)
+{
+	int iFd = 0;
+	int iRet = 0;
+	int iMarks = 0;
+	
+	struct Student stud;
+
+	iFd = open(filename,O_RDONLY);
+	if(iFd==-1)
+	{
+		printf("Error : Unable to open %s\n",filename);
+		return ;
+	}
+	else
+	{
+		while((iRet = read(iFd,&stud,sizeof(stud)))!=0)
+		{
+			if(stud.marks>iMarks)
+			{
+				iMarks = stud.marks;
+			}
+		}
+		close(iFd);
+	}
+	iFd = open(filename,O_RDONLY);
+	if(iFd==-1)
+	{
+	printf("Error : Unable to open %s\n",filename);
+	return ;
+	}
+	else
+	{
+		while((iRet = read(iFd,&stud,sizeof(stud)))!=0)
+		{
+			if(stud.marks==iMarks)
+			{
+				printf("Student name : %s\n",stud.name);
+				printf("Roll no : %d\n",stud.rollno);
+				printf("Marks : %d\n",stud.marks);
+			}
+		}
+	}
+	close(iFd);
+}
